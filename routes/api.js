361f8800b0d4151d27be6f1daa0cf8b0d10f72de
gokulramesh007 <gokulpulari@gmail.com>
2021-01-04 19:15:03 +0530

    server setup

---
+++ routes/api.js
@@ -0,0 +1,28 @@
+const express = require('express');
+
+const router = express.Router();
+
+// Routes
+router.get('/', (req, res) => {
+
+    BlogPost.find({  })
+        .then((data) => {
+            console.log('Data: ', data);
+            res.json(data);
+        })
+        .catch((error) => {
+            console.log('error: ', daerrorta);
+        });
+});
+
+router.get('/name', (req, res) => {
+    const data =  {
+        username: 'peterson',
+        age: 5
+    };
+    res.json(data);
+});
+
+
+
+module.exports = router;
